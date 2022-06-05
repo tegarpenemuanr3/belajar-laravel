24e514c0e47882d40a55dd5603196c78e75f38e7
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-06-05 09:35:45 +0700

    Learn Route Laravel

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,40 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+| https://laravel.com/docs/8.x/routing
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//Route langsung tampilkan konten
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+//Redirect URL routing
+Route::redirect('/coba', '/greeting', 301);
+
+//Route dengan parameter bebas (angka/string)
+Route::get('/user/{id}', function ($id) {
+    return 'User ' . $id;
+});
+
+//Optional Route parameter, pemberian nilai default ketika kosong 
+Route::get('/users/{name?}', function ($name = 'John') {
+    return $name;
+});
+
+//Named route
+Route::get('/coba/profile', function () {
+    return 'Hello Guys';
+})->name('profile');
+
+//Route Prefix
+Route::prefix('admin')->group(function () {
+    Route::get('/coba', function () {
+        // Matches The "/admin/coba" URL
+        return 'Hello Semua';
+    });
 });
